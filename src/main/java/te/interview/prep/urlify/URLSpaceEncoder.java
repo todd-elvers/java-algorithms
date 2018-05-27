f1ee5a8e4c7302ce6c459ba7a3be659a007cd1c9
todd-elvers <todd.elvers@gmail.com>
2018-05-27 15:53:39 -0500

    Optimized impl.

--- src/main/java/te/interview/prep/urlify/URLSpaceEncoder.java
+++ src/main/java/te/interview/prep/urlify/URLSpaceEncoder.java
@@ -7,43 +7,16 @@ public class URLSpaceEncoder {
     }
 
     public String encode(char[] chars, int trueLength) {
-        //TODO: Remove buffer
-        char[] buffer = new char[10];
-        int numBuffChars = 0;
+        int reverseIndex = chars.length - 1;
 
-        for (int i = 0; i < trueLength; i++) {
-            boolean isSpaceChar = chars[i] == ' ';
-            boolean hasBufferChars = numBuffChars > 0;
-
-            if (isSpaceChar) {
-                buffer[numBuffChars++] = '%';
-                buffer[numBuffChars++] = '2';
-                buffer[numBuffChars++] = '0';
+        for (int i = trueLength - 1; i >= 0; i--) {
+            if (Character.isSpaceChar(chars[i])) {
+                chars[reverseIndex--] = '0';
+                chars[reverseIndex--] = '2';
+                chars[reverseIndex--] = '%';
+            } else {
+                chars[reverseIndex--] = chars[i];
             }
-
-            if (hasBufferChars) {
-                // Push the current char onto our buffer (ignoring spaces)
-                if (!isSpaceChar) {
-                    buffer[numBuffChars++] = chars[i];
-                }
-
-                // Pop value from buffer and replace current char with it
-                chars[i] = buffer[0];
-
-                // Shift the buffer over
-                int temp = 0;
-                while (temp < numBuffChars) {
-                    buffer[temp] = buffer[++temp];
-                }
-
-                // Shrink our buffer by one
-                numBuffChars--;
-            }
-        }
-
-        // Write the remaining buffer to the end of the string
-        while(numBuffChars > 0) {
-            chars[(trueLength-1)+numBuffChars] = buffer[--numBuffChars];
         }
 
         return new String(chars);
