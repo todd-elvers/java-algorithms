9b9d7968665d3dd2f69178b733f18fecfec5f309
todd-elvers <todd.elvers@gmail.com>
2018-06-25 18:45:54 -0500

    Created data structures for stacks & queues chapter

---
+++ src/main/java/te/interview/prep/stacks_queues/domain/MyStack.java
@@ -0,0 +1,35 @@
+package te.interview.prep.stacks_queues.domain;
+
+import te.interview.prep.linked_lists.domain.Node;
+
+import java.util.EmptyStackException;
+
+public class MyStack<T> {
+    private Node<T> top;
+
+    public T pop() {
+        if(top == null) throw new EmptyStackException();
+
+        T item = top.data;
+        top = top.next;
+
+        return item;
+    }
+
+    public void push(T item) {
+        Node<T> newTop = new Node<>(item);
+        newTop.next = top;
+        top = newTop;
+    }
+
+    public T peek() {
+        if(top == null) throw new EmptyStackException();
+
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+}
