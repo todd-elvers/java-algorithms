7d65f1cc373db9ac8a392b349fdd9a58bb44bc9a
Todd Elvers <todd.elvers@gmail.com>
2019-03-03 15:54:47 -0600

    Added another test case to BST validator

--- src/test/groovy/te/interview/prep/trees_graphs/BinarySearchTreeValidatorTest.groovy
+++ src/test/groovy/te/interview/prep/trees_graphs/BinarySearchTreeValidatorTest.groovy
@@ -41,4 +41,26 @@ class BinarySearchTreeValidatorTest extends TreeTest {
         then:
             !isBST
     }
+
+    def "can validate large BST"() {
+        given:
+            TreeNode n6 = [6]
+            TreeNode n7 = [7, n6, null]
+            TreeNode n9 = [9]
+            TreeNode n8 = [8, n7, n9]
+            TreeNode n5 = [5, null, n8]
+            TreeNode n11 = [11]
+            TreeNode n10 = [10, n5, n11]
+            TreeNode n12 = [12, n10, null]
+            TreeNode n4 = [4, null, n12]
+            TreeNode n2 = [2]
+            TreeNode n1 = [1, null, n2]
+            TreeNode n3 = [3, n1, n4]
+            TreeNode n18 = [18]
+            TreeNode n14 = [14, null, n18]
+            TreeNode rootOfLargeBST = [13, n3, n14]
+
+        expect:
+            binarySearchTreeValidator.isValidBST(rootOfLargeBST)
+    }
 }
