0b86ff3ecd91454fd8d400dce74f09a9052aec84
Todd Elvers <todd.elvers@gmail.com>
2019-03-27 18:39:22 -0500

    Improved readability of BST inverter slightly

--- src/main/java/te/interview/prep/trees_graphs/BinarySearchTreeInverter.java
+++ src/main/java/te/interview/prep/trees_graphs/BinarySearchTreeInverter.java
@@ -8,13 +8,18 @@ public class BinarySearchTreeInverter {
         if(node == null) return null;
         if(node.isLeafNode()) return node;
 
-        TreeNode temp = node.left;
-        node.left = node.right;
-        node.right = temp;
+        swapLeftAndRight(node);
 
         node.left = invert(node.left);
         node.right = invert(node.right);
+
         return node;
+    }
+
+    private void swapLeftAndRight(TreeNode node) {
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
     }
 
 }
