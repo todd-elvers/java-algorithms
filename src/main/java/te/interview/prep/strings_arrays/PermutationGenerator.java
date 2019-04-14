14af609e883ae2208d45a1f59cd4c2b4c6c9007b
Todd Elvers <todd.elvers@gmail.com>
2019-04-13 21:34:59 -0500

    Algorithm for generating all permutations for a unique set of integers

---
+++ src/main/java/te/interview/prep/strings_arrays/PermutationGenerator.java
@@ -0,0 +1,30 @@
+package te.interview.prep.strings_arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/permutations/">Problem on leetcode</a>
+ */
+public class PermutationGenerator {
+
+    public List<List<Integer>> generate(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        backtrack(results, new ArrayList<>(), nums);
+        return results;
+    }
+
+    private void backtrack(List<List<Integer>> results, ArrayList<Integer> temp, int[] nums) {
+        if (temp.size() == nums.length) {
+            results.add(new ArrayList<>(temp));
+        } else {
+            for (int number : nums) {
+                if (temp.contains(number)) continue;   // Ensures uniqueness
+                temp.add(number);
+                backtrack(results, temp, nums);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+
+}
---
+++ src/test/groovy/te/interview/prep/strings_arrays/PermutationGeneratorTest.groovy
@@ -0,0 +1,23 @@
+package te.interview.prep.strings_arrays
+
+import spock.lang.Specification
+
+class PermutationGeneratorTest extends Specification {
+
+    PermutationGenerator permutationGenerator = []
+
+    def "can generate all permutations from unique set of integers"() {
+        when:
+            List<List<Integer>> permutations = permutationGenerator.generate([1, 2, 3] as int[])
+
+        then:
+            permutations?.size() == 6
+            [1, 2, 3] in permutations
+            [1, 3, 2] in permutations
+            [2, 1, 3] in permutations
+            [2, 3, 1] in permutations
+            [3, 1, 2] in permutations
+            [3, 2, 1] in permutations
+    }
+
+}
