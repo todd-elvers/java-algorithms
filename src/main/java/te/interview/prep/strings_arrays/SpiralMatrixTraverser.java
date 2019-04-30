c9af06acb05e056e107d8f7dbfdc7557c5c1b9e1
Todd Elvers <todd.elvers@gmail.com>
2019-04-29 19:39:41 -0500

    Added skeleton for spiral matrix problem

---
+++ src/main/java/te/interview/prep/strings_arrays/SpiralMatrixTraverser.java
@@ -0,0 +1,16 @@
+package te.interview.prep.strings_arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/spiral-matrix/">Problem on leetcode</a>
+ */
+//TODO: Solve this
+public class SpiralMatrixTraverser {
+
+    public List<Integer> traverse(int[][] matrix) {
+        return new ArrayList<>();
+    }
+
+}
---
+++ src/test/groovy/te/interview/prep/strings_arrays/SpiralMatrixTraverserTest.groovy
@@ -0,0 +1,21 @@
+package te.interview.prep.strings_arrays
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class SpiralMatrixTraverserTest extends Specification {
+
+    @Subject
+    SpiralMatrixTraverser spiralMatrixTraverser = []
+
+    def "can traverse a matrix in spiral order"(int[][] matrix, List<Integer> spiralOrder) {
+        expect:
+            spiralMatrixTraverser.traverse(matrix) == spiralOrder
+
+        where:
+            matrix                                        || spiralOrder
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]]             || [1, 2, 3, 6, 9, 8, 7, 4, 5]
+            [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]] || [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+    }
+
+}
