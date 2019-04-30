ff7343f460bcde6616529fc748c6e455acef82e6
Todd Elvers <todd.elvers@gmail.com>
2019-04-30 14:06:39 -0500

    Algorithm to traverse a 2D matrix in spiral order

--- src/main/java/te/interview/prep/strings_arrays/SpiralMatrixTraverser.java
+++ src/main/java/te/interview/prep/strings_arrays/SpiralMatrixTraverser.java
@@ -1,16 +1,45 @@
 package te.interview.prep.strings_arrays;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * @see <a href="https://leetcode.com/problems/spiral-matrix/">Problem on leetcode</a>
  */
-//TODO: Solve this
 public class SpiralMatrixTraverser {
 
     public List<Integer> traverse(int[][] matrix) {
-        return new ArrayList<>();
+        if (matrix == null || matrix.length == 0) return Collections.emptyList();
+
+        List<Integer> results = new ArrayList<>();
+        traverseMatrix(results, matrix, new boolean[matrix.length][matrix[0].length], 0, 0);
+        return results;
     }
 
+    /**
+     * Algorithm, in order:
+     *      - Go right as far as possible
+     *      - Go down as far as possible
+     *      - Go left as far as possible
+     *      - Go up as far as possible (edge case: don't go right before you've gone all the way up)
+     */
+    private void traverseMatrix(List<Integer> results, int[][] matrix, boolean[][] visited, int row, int column) {
+        if (row < 0 || row > matrix.length - 1 || column < 0 || column > matrix[row].length - 1 || visited[row][column]) {
+            return;
+        }
+
+        visited[row][column] = true;
+
+        results.add(matrix[row][column]);
+
+        // Edge case: only go right if we're on the first row or the row above is already visited
+        if (row == 0 || visited[row - 1][column]) {
+            traverseMatrix(results, matrix, visited, row, column + 1);
+        }
+
+        traverseMatrix(results, matrix, visited, row + 1, column);
+        traverseMatrix(results, matrix, visited, row, column - 1);
+        traverseMatrix(results, matrix, visited, row - 1, column);
+    }
 }
--- src/test/groovy/te/interview/prep/strings_arrays/SpiralMatrixTraverserTest.groovy
+++ src/test/groovy/te/interview/prep/strings_arrays/SpiralMatrixTraverserTest.groovy
@@ -1,6 +1,6 @@
 package te.interview.prep.strings_arrays
 
-import groovy.transform.NotYetImplemented
+
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -9,15 +9,16 @@ class SpiralMatrixTraverserTest extends Specification {
     @Subject
     SpiralMatrixTraverser spiralMatrixTraverser = []
 
-    @NotYetImplemented
     def "can traverse a matrix in spiral order"(int[][] matrix, List<Integer> spiralOrder) {
         expect:
             spiralMatrixTraverser.traverse(matrix) == spiralOrder
 
         where:
-            matrix                                        || spiralOrder
-            [[1, 2, 3], [4, 5, 6], [7, 8, 9]]             || [1, 2, 3, 6, 9, 8, 7, 4, 5]
-            [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]] || [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+            matrix                                                                                          || spiralOrder
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]]                                                               || [1, 2, 3, 6, 9, 8, 7, 4, 5]
+            [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]                                                   || [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+            [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]] || [1, 2, 3, 4, 5, 6, 12, 18, 24, 23, 22, 21, 20, 19, 13, 7, 8, 9, 10, 11, 17, 16, 15, 14]
     }
+
 
 }
