0b9389149da9f7a2a1f4d487466aa12b0efb079a
Todd Elvers <todd.elvers@gmail.com>
2019-05-09 14:18:12 -0500

    Algorithm for counting the number of subarrays in an array with sums equal to k

--- src/main/java/te/interview/prep/strings_arrays/SubarraySumFinder.java
+++ src/main/java/te/interview/prep/strings_arrays/SubarraySumFinder.java
@@ -1,13 +1,69 @@
 package te.interview.prep.strings_arrays;
 
+import java.util.HashMap;
+
 /**
  * @see <a href="https://leetcode.com/problems/subarray-sum-equals-k/">Problem on leetcode</a>
  */
-//TODO: Solve this
+//TODO: Review this: re-create edge cases & re-create solution
 public class SubarraySumFinder {
 
-    public int countSubarraysThatSumToK(int[] nums, int k) {
-        return 0;
+    // Time: O(n^2), Space: O(n)
+    static class RecursiveApproach {
+        public int countSubarraysThatSumToK(int[] nums, int k) {
+            int count = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                count += howManyValuesToTheRightSumToK(nums, k, nums[i], i + 1);
+            }
+
+            return count;
+        }
+
+        private int howManyValuesToTheRightSumToK(int[] nums, int targetSum, int runningSum, int index) {
+            int currentSumCount = (runningSum == targetSum) ? 1 : 0;
+
+            if (index <= nums.length - 1) {
+                currentSumCount += howManyValuesToTheRightSumToK(
+                        nums,
+                        targetSum,
+                        runningSum + nums[index],
+                        index + 1
+                );
+            }
+
+            return currentSumCount;
+        }
     }
+
+    // Time: O(n), Space: O(n)
+    static class HashMapApproach {
+        
+        public int countSubarraysThatSumToK(int[] nums, int k) {
+            int sum = 0, subarraySumsEqualToK = 0;
+
+            HashMap<Integer, Integer> previousSumOccurrences = new HashMap<>();
+            // This ensures that if sum-k=0 (i.e. sum == k) that we increment our sum count
+            previousSumOccurrences.put(0, 1);
+
+            for(int number : nums) {
+                sum += number;
+
+                // If the difference between any previous sum and k is a sum
+                // we've already encountered then the sum of the values between
+                // that previous sum and the current value we're on is also k.
+                // So we increment our count by how many times we've seen the previous sum.
+                if(previousSumOccurrences.containsKey(sum - k)) {
+                    subarraySumsEqualToK += previousSumOccurrences.get(sum - k);
+                }
+
+                int occurrencesOfThisSum = previousSumOccurrences.getOrDefault(sum, 0) + 1;
+                previousSumOccurrences.put(sum, occurrencesOfThisSum);
+            }
+
+            return subarraySumsEqualToK;
+        }
+    }
+
 
 }
--- src/test/groovy/te/interview/prep/strings_arrays/SubarraySumFinderTest.groovy
+++ src/test/groovy/te/interview/prep/strings_arrays/SubarraySumFinderTest.groovy
@@ -1,22 +1,28 @@
 package te.interview.prep.strings_arrays
 
-import groovy.transform.NotYetImplemented
+
 import spock.lang.Specification
 import spock.lang.Subject
 
 class SubarraySumFinderTest extends Specification {
 
     @Subject
-    SubarraySumFinder subarraySumFinder = []
+    SubarraySumFinder.RecursiveApproach recursiveSubarraySumFinder = []
 
-    @NotYetImplemented
+    @Subject
+    SubarraySumFinder.HashMapApproach hashMapSubarraySumFinder = []
+
     def "can find the number of continuous subarrays that sum to a given value"(int[] nums, int k, int result) {
         expect:
-            subarraySumFinder.countSubarraysThatSumToK(nums, k) == result
+            recursiveSubarraySumFinder.countSubarraysThatSumToK(nums, k) == result
+            hashMapSubarraySumFinder.countSubarraysThatSumToK(nums, k) == result
 
         where:
-            nums      | k || result
-            [1, 1, 1] | 2 || 2
+            nums                     | k || result
+            [0, 0, 0]                | 0 || 6
+            [1, 1, 1]                | 2 || 2
+            []                       | 0 || 0
+            [3, 4, 7, -1, -3, 4, -1] | 7 || 4
     }
 
 }
