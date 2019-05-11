f59fcc099f91f96af67c05557d43ab75e6f45d95
todd-elvers <todd.elvers@gmail.com>
2019-05-11 00:09:22 -0500

    Added skeleton for tinyURL problem

---
+++ src/main/java/te/interview/prep/strings_arrays/TinyUrlService.java
@@ -0,0 +1,18 @@
+package te.interview.prep.strings_arrays;
+
+/**
+ * @see <a href="https://leetcode.com/problems/encode-and-decode-tinyurl/">Problem on leetcode</a>
+ */
+public class TinyUrlService {
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        return "";
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return "";
+    }
+
+}
---
+++ src/test/groovy/te/interview/prep/strings_arrays/TinyUrlServiceTest.groovy
@@ -0,0 +1,21 @@
+package te.interview.prep.strings_arrays
+
+import groovy.transform.NotYetImplemented
+import spock.lang.Specification
+import spock.lang.Subject
+
+class TinyUrlServiceTest extends Specification {
+
+    @Subject
+    TinyUrlService tinyUrlService = []
+
+    @NotYetImplemented
+    def "can encode and decode urls to/from tiny urls"() {
+        expect:
+            tinyUrlService.decode(tinyUrlService.encode(url)) == url
+
+        where:
+            url << ["https://www.google.com", "www.google.com"]
+    }
+
+}
