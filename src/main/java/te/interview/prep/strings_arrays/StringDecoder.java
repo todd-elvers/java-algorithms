5399d6d73356280f4a2a03d4205ff3168fba599a
Todd Elvers <todd.elvers@gmail.com>
2019-05-11 20:36:42 -0500

    Algorithm for decoding a string

--- src/main/java/te/interview/prep/strings_arrays/StringDecoder.java
+++ src/main/java/te/interview/prep/strings_arrays/StringDecoder.java
@@ -3,11 +3,72 @@ package te.interview.prep.strings_arrays;
 /**
  * @see <a href="https://leetcode.com/problems/decode-string/">Problem on leetcode</a>
  */
-//TODO: Solve this
 public class StringDecoder {
 
-    public String decodeString(String s) {
-        return s;
+    // Space: O(n), Time: O(n)  where n is the length of the decoded string
+    public String decodeString(String input) {
+        if(input == null) return null;
+
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i < input.length(); i++) {
+            if(Character.isDigit(input.charAt(i))) {
+                int startingBracketPos = findEncodingStartBracket(input, i);
+                int timesEncoded = Integer.parseInt(input.substring(i, startingBracketPos));
+                int endingBracketPos = findEncodingEndBracket(input, startingBracketPos);
+                output.append(
+                        decode(input, timesEncoded, startingBracketPos, endingBracketPos)
+                );
+                i = endingBracketPos;
+            } else {
+                output.append(input.charAt(i));
+            }
+        }
+
+        return output.toString();
+    }
+
+    private StringBuilder decode(String input, int timesEncoded, int pos, int endingBracketPos) {
+        StringBuilder decodedContents = new StringBuilder();
+
+        while(++pos != endingBracketPos) {
+            char c = input.charAt(pos);
+
+            if(Character.isDigit(c)) {
+                int startOfEncoding = findEncodingStartBracket(input, pos);
+                int nestTimesEncoded = Integer.parseInt(input.substring(pos, startOfEncoding));
+                int endOfEncoding = findEncodingEndBracket(input, startOfEncoding);
+                decodedContents.append(
+                        decode(input, nestTimesEncoded, startOfEncoding, endOfEncoding)
+                );
+                pos = endOfEncoding;
+            } else {
+                decodedContents.append(c);
+            }
+        }
+
+        String decodedBase = decodedContents.toString();
+        for(int i = 1; i < timesEncoded; i++) {
+            decodedContents.append(decodedBase);
+        }
+
+        return decodedContents;
+    }
+
+    private int findEncodingStartBracket(String input, int pos) {
+        while(input.charAt(++pos) != '[') { }
+        return pos;
+    }
+
+    private int findEncodingEndBracket(String input, int pos) {
+        int numOpen = 1;
+        while(numOpen != 0) {
+            switch(input.charAt(++pos)) {
+                case ']': numOpen--; break;
+                case '[': numOpen++; break;
+            }
+        }
+
+        return pos;
     }
 
 }
--- src/test/groovy/te/interview/prep/strings_arrays/StringDecoderTest.groovy
+++ src/test/groovy/te/interview/prep/strings_arrays/StringDecoderTest.groovy
@@ -1,6 +1,6 @@
 package te.interview.prep.strings_arrays
 
-import groovy.transform.NotYetImplemented
+
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -9,16 +9,17 @@ class StringDecoderTest extends Specification {
     @Subject
     StringDecoder decoder = []
 
-    @NotYetImplemented
     def "can decode encoded strings"() {
         expect:
             decoder.decodeString(encoded) == decoded
 
         where:
-            encoded         || decoded
-            "3[a]2[bc]"     || "aaabcbc"
-            "3[a2[c]]"      || "accaccacc"
-            "2[abc]3[cd]ef" || "abcabccdcdcdef"
+            encoded          || decoded
+            null             || null
+            "3[a]2[bc]"      || "aaabcbc"
+            "x3[a2[c]]1[x]d" || "xaccaccaccxd"
+            "2[abc]3[cd]ef"  || "abcabccdcdcdef"
+            "10[a]"          || "aaaaaaaaaa"
     }
 
 }
