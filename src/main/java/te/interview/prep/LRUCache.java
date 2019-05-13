05c19d78d0e21a25f41ad6457bf337f68451b5d0
todd-elvers <todd.elvers@gmail.com>
2019-05-13 17:39:40 -0500

    Improved LRUCache to O(1) time complexity

--- src/main/java/te/interview/prep/LRUCache.java
+++ src/main/java/te/interview/prep/LRUCache.java
@@ -6,52 +6,93 @@ import java.util.Map;
 /**
  * @see <a href="https://leetcode.com/problems/lru-cache/">Problem on leetcode</a>
  */
-//TODO: Review this and make it O(1) by using doubly-linked list + map of int->node
 public class LRUCache {
-    private int clock;
     private int capacity;
-    private Map<Integer, TimeAwareValue> cache;
+    private Map<Integer, Node> cache;
+    private Node head, tail;
 
     public LRUCache(int capacity) {
         this.cache = new HashMap<>(capacity);
         this.capacity = capacity;
-        this.clock = 0;
     }
 
     public int get(int key) {
-        if(!cache.containsKey(key)) return -1;
+        if (!cache.containsKey(key)) return -1;
+        moveNodeToHead(cache.get(key));
+        return head.value;
+    }
 
-        TimeAwareValue cachedValue = cache.get(key);
-        cachedValue.lastTimeUsed = ++clock;
-        cache.put(key, cachedValue);
+    // Move an existing node from wherever it is to the head
+    private void moveNodeToHead(Node node) {
+        if(node == head) return;
 
-        return cachedValue.value;
+        // If our node isn't the head then it's the tail or somewhere in between
+        if (node == tail) {
+            truncateTailByOne();
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+
+        setNodeAsHead(node);
+    }
+
+    private void truncateTailByOne() {
+        tail = tail.prev;
+        tail.next = null;
+    }
+
+    private void setNodeAsHead(Node node) {
+        // Edge case: cache may be empty
+        if (head == null) {
+            head = tail = node;
+        } else {
+            // Prepare our node to be the new head
+            node.prev = null;
+            node.next = head;
+
+            // Push the existing head node down
+            head.prev = node;
+            head = node;
+        }
     }
 
     public void put(int key, int value) {
-        // Edge case: only evict the LRU if we're at capacity and don't already have an entry for `key`
-        if(!cache.containsKey(key) && cache.size() == capacity) {
-            evictLeastRecentlyUsedValue();
+        if (cache.containsKey(key)) {
+            moveNodeToHead(cache.get(key).withNewValue(value));
+        } else {
+            if (cache.size() == capacity) {
+                evictLeastRecentlyUsed();
+            }
+            setNodeAsHead(new Node(key, value));
         }
 
-        cache.put(key, new TimeAwareValue(value, ++clock));
+        cache.put(key, head);
     }
 
-    private void evictLeastRecentlyUsedValue() {
-        Integer keyToLRU = cache.entrySet().stream()
-                .reduce((e1, e2) -> e1.getValue().lastTimeUsed > e2.getValue().lastTimeUsed ? e2 : e1)
-                .map(Map.Entry::getKey)
-                .get();
+    private void evictLeastRecentlyUsed() {
+        cache.remove(tail.key);
 
-        cache.remove(keyToLRU);
+        // LRU is either the tail of the list or both the head AND the tail
+        if (capacity == 1) {
+            head = tail = null;
+        } else {
+            truncateTailByOne();
+        }
     }
 
-    private static class TimeAwareValue {
-        int value, lastTimeUsed;
-        TimeAwareValue(int value, int lastTimeUsed) {
+    private class Node {
+        int key, value;
+        Node next, prev;
+
+        Node(int key, int value) {
+            this.key = key;
             this.value = value;
-            this.lastTimeUsed = lastTimeUsed;
         }
 
+        Node withNewValue(int value) {
+            this.value = value;
+            return this;
+        }
     }
 }
--- src/test/groovy/te/interview/prep/LRUCacheTest.groovy
+++ src/test/groovy/te/interview/prep/LRUCacheTest.groovy
@@ -1,6 +1,5 @@
 package te.interview.prep
 
-
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -10,34 +9,231 @@ class LRUCacheTest extends Specification {
     LRUCache cache
 
     def "can keep a cache that discards the least recently used elements first"() {
-        given: 'a cache that can only hold 2 elements'
-            cache = [2]
+        given:
+            cache = [10]
 
-        when: 'we insert 1 & 2'
-            cache.put(1, 1)
-            cache.put(2, 2)  // TODO: Can we move this insert down to the next 'when'?
+        when:
+            cache.put(10, 13)
+            cache.put(3, 17)
+            cache.put(6, 11)
+            cache.put(10, 5)
+            cache.put(9, 10)
 
-        then: 'we can find 1'
-            cache.get(1) == 1
+        then:
+            cache.get(13) == -1
 
-        when: 'we insert 3'
-            cache.put(3, 3)
+        when:
+            cache.put(2, 19)
 
-        then: 'we evicted 2'
-            cache.get(2) == -1
+        then:
+            cache.get(2) == 19
+            cache.get(3) == 17
 
-        when: 'we insert 4'
-            cache.put(4, 4)
+        when:
+            cache.put(5, 25)
 
-        and: 'update our 4 to now be a 5, but with the same key'
-            cache.put(4, 5)
+        then:
+            cache.get(8) == -1
 
-        then: 'we evicted 1'
-            cache.get(1) == -1
+        when:
+            cache.put(9, 22)
+            cache.put(5, 5)
+            cache.put(1, 30)
 
-        expect: 'our cache to only contain 3 & 4 at this point'
-            cache.get(3) == 3
-            cache.get(4) == 5
+        then:
+            cache.get(11) == -1
+
+        when:
+            cache.put(9, 12)
+
+        then:
+            cache.get(7) == -1
+            cache.get(5) == 5
+            cache.get(8) == -1
+            cache.get(9) == 12
+
+        when:
+            cache.put(4, 30)
+            cache.put(9, 3)
+
+        then:
+            cache.get(9) == 3
+            cache.get(10) == 5
+            cache.get(10) == 5
+
+        when:
+            cache.put(6, 14)
+            cache.put(3, 1)
+
+        then:
+            cache.get(3) == 1
+
+        when:
+            cache.put(10, 11)
+
+        then:
+            cache.get(8) == -1
+
+        when:
+            cache.put(2, 14)
+
+        then:
+            cache.get(1) == 30
+            cache.get(5) == 5
+            cache.get(4) == 30
+
+        when:
+            cache.put(11, 4)
+            cache.put(12, 24)
+            cache.put(5, 18)
+
+        then:
+            cache.get(13) == -1
+
+        when:
+            cache.put(7, 23)
+
+        then:
+            cache.get(8) == -1
+            cache.get(12) == 24
+
+        when:
+            cache.put(3, 27)
+            cache.put(2, 12)
+
+        then:
+            cache.get(5) == 18
+
+        when:
+            cache.put(2, 9)
+            cache.put(13, 4)
+            cache.put(8, 18)
+            cache.put(1, 7)
+
+        then:
+            cache.get(6) == -1
+
+        when:
+            cache.put(9, 29)
+            cache.put(8, 21)
+
+        then:
+            cache.get(5) == 18
+
+        when:
+            cache.put(6, 30)
+            cache.put(1, 12)
+
+        then:
+            cache.get(10) == -1
+
+        when:
+            cache.put(4, 15)
+            cache.put(7, 22)
+            cache.put(11, 26)
+            cache.put(8, 17)
+            cache.put(9, 29)
+
+        then:
+            cache.get(5) == 18
+
+        when:
+            cache.put(3, 4)
+            cache.put(11, 30)
+
+        then:
+            cache.get(12) == -1
+
+        when:
+            cache.put(4, 29)
+
+        then:
+            cache.get(3) == 4
+            cache.get(9) == 29
+            cache.get(6) == 30
+
+        when:
+            cache.put(3, 4)
+
+        then:
+            cache.get(1) == 12
+            cache.get(10) == -1
+
+        when:
+            cache.put(3, 29)
+            cache.put(10, 28)
+            cache.put(1, 20)
+            cache.put(11, 13)
+
+        then:
+            cache.get(3) == 29
+
+        when:
+            cache.put(3, 12)
+            cache.put(3, 8)
+            cache.put(10, 9)
+            cache.put(3, 26)
+
+        then:
+            cache.get(8) == 17
+            cache.get(7) == 22
+            cache.get(5) == 18
+
+        when:
+            cache.put(13, 17)
+            cache.put(2, 27)
+            cache.put(11, 15)
+
+        then:
+            cache.get(12) == -1
+
+        when:
+            cache.put(9, 19)
+            cache.put(2, 15)
+            cache.put(3, 16)
+
+        then:
+            cache.get(1) == 20
+
+        when:
+            cache.put(12, 17)
+            cache.put(9, 1)
+            cache.put(6, 19)
+
+        then:
+            cache.get(4) == -1
+            cache.get(5) == 18
+            cache.get(5) == 18
+
+        when:
+            cache.put(8, 1)
+            cache.put(11, 7)
+            cache.put(5, 2)
+            cache.put(9, 28)
+
+        then:
+            cache.get(1) == 20
+
+        and:
+            cache.put(2, 2)
+            cache.put(7, 4)
+            cache.put(4, 22)
+            cache.put(7, 24)
+            cache.put(9, 26)
+            cache.put(13, 28)
+            cache.put(11, 26)
     }
 
+    def "can handle a cache of size 1"() {
+        given:
+            cache = [1]
+
+        when:
+            cache.put(1, 1)
+            cache.put(2, 2)
+
+        then:
+            cache.get(2) == 2
+            cache.get(1) == -1
+    }
 }
