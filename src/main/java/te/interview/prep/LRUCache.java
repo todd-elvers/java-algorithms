1d4d91c1de00af31b25a549af950a7f62e247c68
todd-elvers <todd.elvers@gmail.com>
2019-05-13 12:34:52 -0500

    Algorithm for LRU in O(n)

--- src/main/java/te/interview/prep/LRUCache.java
+++ src/main/java/te/interview/prep/LRUCache.java
@@ -1,21 +1,57 @@
 package te.interview.prep;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @see <a href="https://leetcode.com/problems/lru-cache/">Problem on leetcode</a>
  */
-//TODO: Solve this
+//TODO: Review this and make it O(1) by using doubly-linked list + map of int->node
 public class LRUCache {
+    private int clock;
+    private int capacity;
+    private Map<Integer, TimeAwareValue> cache;
 
     public LRUCache(int capacity) {
-
+        this.cache = new HashMap<>(capacity);
+        this.capacity = capacity;
+        this.clock = 0;
     }
 
     public int get(int key) {
-        return -1;
+        if(!cache.containsKey(key)) return -1;
+
+        TimeAwareValue cachedValue = cache.get(key);
+        cachedValue.lastTimeUsed = ++clock;
+        cache.put(key, cachedValue);
+
+        return cachedValue.value;
     }
 
     public void put(int key, int value) {
+        // Edge case: only evict the LRU if we're at capacity and don't already have an entry for `key`
+        if(!cache.containsKey(key) && cache.size() == capacity) {
+            evictLeastRecentlyUsedValue();
+        }
 
+        cache.put(key, new TimeAwareValue(value, ++clock));
     }
 
+    private void evictLeastRecentlyUsedValue() {
+        Integer keyToLRU = cache.entrySet().stream()
+                .reduce((e1, e2) -> e1.getValue().lastTimeUsed > e2.getValue().lastTimeUsed ? e2 : e1)
+                .map(Map.Entry::getKey)
+                .get();
+
+        cache.remove(keyToLRU);
+    }
+
+    private static class TimeAwareValue {
+        int value, lastTimeUsed;
+        TimeAwareValue(int value, int lastTimeUsed) {
+            this.value = value;
+            this.lastTimeUsed = lastTimeUsed;
+        }
+
+    }
 }
--- src/test/groovy/te/interview/prep/LRUCacheTest.groovy
+++ src/test/groovy/te/interview/prep/LRUCacheTest.groovy
@@ -1,6 +1,6 @@
 package te.interview.prep
 
-import groovy.transform.NotYetImplemented
+
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -9,14 +9,13 @@ class LRUCacheTest extends Specification {
     @Subject
     LRUCache cache
 
-    @NotYetImplemented
     def "can keep a cache that discards the least recently used elements first"() {
         given: 'a cache that can only hold 2 elements'
             cache = [2]
 
         when: 'we insert 1 & 2'
-            cache.put(1,1)
-            cache.put(2,2)  // TODO: Can we move this insert down to the next 'when'?
+            cache.put(1, 1)
+            cache.put(2, 2)  // TODO: Can we move this insert down to the next 'when'?
 
         then: 'we can find 1'
             cache.get(1) == 1
@@ -30,12 +29,15 @@ class LRUCacheTest extends Specification {
         when: 'we insert 4'
             cache.put(4, 4)
 
+        and: 'update our 4 to now be a 5, but with the same key'
+            cache.put(4, 5)
+
         then: 'we evicted 1'
             cache.get(1) == -1
 
         expect: 'our cache to only contain 3 & 4 at this point'
             cache.get(3) == 3
-            cache.get(4) == 4
+            cache.get(4) == 5
     }
 
 }
