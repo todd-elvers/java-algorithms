ac9adcfd96c2313a8d0d15d8b98450307d47816e
Todd Elvers <todd.elvers@gmail.com>
2019-05-19 19:49:15 -0500

    Corrected bug in IslandCounter2

--- src/main/java/te/interview/prep/strings_arrays/IslandCounter2.java
+++ src/main/java/te/interview/prep/strings_arrays/IslandCounter2.java
@@ -8,70 +8,68 @@ import java.util.Set;
 /**
  * @see <a href="https://leetcode.com/problems/number-of-islands-ii/">Problem on leetcode</a>
  */
-//TODO: Review come back to this and use Union Find; my logic doesn't work for one of the edge cases
+//TODO: Review this later & try using Union Find
 public class IslandCounter2 {
 
     public List<Integer> count(int m, int n, int[][] positions) {
-        if (m <= 0 || n <= 0 || positions.length == 0 || positions[0].length == 0)
-            return new ArrayList<>();
+        if (m <= 0 || n <= 0 || positions.length == 0 || positions[0].length == 0) return new ArrayList<>();
 
         int[][] grid = new int[m][n];
-        int islandCount = 0;
+        int curIslandCount = 0, islandID = 1;
         List<Integer> islandCountByIteration = new ArrayList<>();
+
         for (int[] position : positions) {
-            islandCount += countIslandDifferences(grid, position[0], position[1], islandCount);
-            islandCountByIteration.add(islandCount);
+            int adjustment = findIslandCountAdjustment(grid, position[0], position[1], islandID);
+            if (adjustment == 1) islandID++;
+            islandCountByIteration.add(curIslandCount += adjustment);
         }
 
         return islandCountByIteration;
     }
 
-    private void getNeighboringLandValues(Set<Integer> connectedLandValues, int[][] grid, int row, int column) {
-        if (row + 1 <= grid.length - 1) connectedLandValues.add(grid[row + 1][column]);
-        if (row - 1 >= 0) connectedLandValues.add(grid[row - 1][column]);
-        if (column + 1 <= grid[row].length - 1) connectedLandValues.add(grid[row][column + 1]);
-        if (column - 1 >= 0) connectedLandValues.add(grid[row][column - 1]);
-
-        connectedLandValues.remove(0);
-    }
-
-    private int countIslandDifferences(int[][] grid, int row, int column, int islandCounter) {
+    private int findIslandCountAdjustment(int[][] grid, int row, int column, int islandID) {
         if (grid[row][column] != 0) return 0;
 
-        Set<Integer> connectedLandValues = new HashSet<>();
-        getNeighboringLandValues(connectedLandValues, grid, row, column);
-        if (connectedLandValues.isEmpty()) {
-            // If we don't add some uniqueness here we'll fail we may fail to detect
-            // the correct number of neighboring land values later on
-            grid[row][column] = ++islandCounter;
+        Set<Integer> connectedLandValues = getNeighboringLandValues(grid, row, column);
+
+        if(connectedLandValues.isEmpty()) {
+            grid[row][column] = islandID;
             return 1;
         } else if (connectedLandValues.size() == 1) {
             grid[row][column] = connectedLandValues.iterator().next();
             return 0;
         } else {
-            int minValue = connectedLandValues.stream().min(Integer::compareTo).get();
             grid[row][column] = -1;
-            setAllConnectedLands(grid, row, column, minValue);
+            mergeIslands(grid, row, column, connectedLandValues.iterator().next());
             return -(connectedLandValues.size() - 1);
         }
     }
 
-    private void setAllConnectedLands(int[][] grid, int row, int column, int value) {
-        if (row < 0
-                || row > grid.length - 1
-                || column < 0
-                || column > grid[row].length - 1
-                || grid[row][column] == 0
-                || grid[row][column] == value) {
-            return;
-        }
+    private Set<Integer> getNeighboringLandValues(int[][] grid, int row, int column) {
+        Set<Integer> neighbors = new HashSet<>();
 
-        grid[row][column] = value;
+        neighbors.add(getValue(grid, row + 1, column));
+        neighbors.add(getValue(grid, row - 1, column));
+        neighbors.add(getValue(grid, row, column + 1));
+        neighbors.add(getValue(grid, row, column - 1));
+        neighbors.remove(0);
 
-        setAllConnectedLands(grid, row + 1, column, value);
-        setAllConnectedLands(grid, row - 1, column, value);
-        setAllConnectedLands(grid, row, column - 1, value);
-        setAllConnectedLands(grid, row, column + 1, value);
+        return neighbors;
     }
 
+    private void mergeIslands(int[][] grid, int row, int column, int value) {
+        if (getValue(grid, row, column) == 0 || getValue(grid, row, column) == value) return;
+
+        grid[row][column] = value;
+        mergeIslands(grid, row + 1, column, value);
+        mergeIslands(grid, row - 1, column, value);
+        mergeIslands(grid, row, column - 1, value);
+        mergeIslands(grid, row, column + 1, value);
+    }
+
+    private int getValue(int[][] grid, int row, int column) {
+        return row < 0 || row > grid.length - 1 || column < 0 || column > grid[row].length - 1
+                ? 0
+                : grid[row][column];
+    }
 }
