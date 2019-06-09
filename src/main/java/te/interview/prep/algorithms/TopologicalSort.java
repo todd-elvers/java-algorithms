688a6c578a04bcc535657b5473a63a6294f86c8a
Todd Elvers <todd.elvers@gmail.com>
2019-06-09 16:23:23 -0500

    Implemented ToplogicalSort

---
+++ src/main/java/te/interview/prep/algorithms/TopologicalSort.java
@@ -0,0 +1,106 @@
+package te.interview.prep.algorithms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class TopologicalSort {
+
+    public List<Vertex> sort(Graph g) {
+        // Count the number of incoming edges to each vertex
+        Map<Vertex, Integer> inboundEdgeCounts = new HashMap<>();
+        for (Vertex v : g.vertices) {
+            inboundEdgeCounts.putIfAbsent(v, 0);
+            for (Vertex u : v.vertices) {
+                inboundEdgeCounts.put(u, inboundEdgeCounts.getOrDefault(u, 0) + 1);
+            }
+        }
+
+        // Queue up all vertices with no incoming edges
+        LinkedList<Vertex> queue = new LinkedList<>();
+        for (Map.Entry<Vertex, Integer> entry : inboundEdgeCounts.entrySet()) {
+            if (entry.getValue() == 0) {
+                queue.addLast(entry.getKey());
+            }
+        }
+
+        // Process all vertices with no inbound edges, decrementing the inbound edges of
+        // all neighboring vertices and adding them to the queue if their inbound count is 0
+        List<Vertex> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Vertex v = queue.removeFirst();
+            for (Vertex u : v.vertices) {
+                int newEdgeCount = inboundEdgeCounts.compute(u, (vertex, count) -> count - 1);
+                if (newEdgeCount == 0) {
+                    queue.addLast(u);
+                }
+            }
+
+            result.add(v);
+        }
+
+        return result;
+    }
+
+    public Map<Vertex, Integer> findShortestPathsToAllOtherVertices(Graph g, Vertex source) {
+        List<Vertex> sorted = sort(g);
+
+        // Initialize distance to all nodes from source as infinity (except for source)
+        Map<Vertex, Integer> distanceFromSource = new HashMap<>(sorted.size());
+        for(Vertex v : sorted) distanceFromSource.put(v, Integer.MAX_VALUE);
+        distanceFromSource.put(source, 0);
+
+        for (Vertex u : sorted) {
+            // All nodes prior to `source` will remain infinitely far away
+            if(distanceFromSource.get(u) == Integer.MAX_VALUE) continue;
+
+            for (Vertex v : u.vertices) {
+                // Update our shortest path estimate if our previous source -> v
+                // estimate is more expensive than our current source -> v estimate
+                int pathCost = distanceFromSource.get(u) + u.edgeWeights.get(v);
+                if(distanceFromSource.get(v) > pathCost) {
+                    distanceFromSource.put(v, pathCost);
+                }
+            }
+        }
+
+        return distanceFromSource;
+    }
+
+
+    protected static class Graph {
+        List<Vertex> vertices = new ArrayList<>();
+    }
+
+    protected static class Vertex {
+        List<Vertex> vertices = new ArrayList<>();
+        Map<Vertex, Integer> edgeWeights = new HashMap<>();
+        String name;
+
+        public Vertex(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Vertex)) return false;
+            Vertex vertex = (Vertex) o;
+            return name.equals(vertex.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+}
---
+++ src/test/groovy/te/interview/prep/algorithms/TopologicalSortTest.groovy
@@ -0,0 +1,102 @@
+package te.interview.prep.algorithms
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class TopologicalSortTest extends Specification {
+
+    @Subject
+    TopologicalSort algorithm = []
+
+    def "can topologically sort unweighted DAG"() {
+        given: 'a graph and a bunch of vertices'
+            TopologicalSort.Graph g = []
+            def v1 = new TopologicalSort.Vertex('A')
+            def v2 = new TopologicalSort.Vertex('B')
+            def v3 = new TopologicalSort.Vertex('C')
+            def v4 = new TopologicalSort.Vertex('D')
+            def v5 = new TopologicalSort.Vertex('E')
+            def v6 = new TopologicalSort.Vertex('F')
+            def v7 = new TopologicalSort.Vertex('G')
+            def v8 = new TopologicalSort.Vertex('H')
+            def v9 = new TopologicalSort.Vertex('I')
+            g.vertices.addAll([v9, v1, v8, v2, v7, v3, v6, v4, v5])
+
+        and: 'B -> [C, E]'
+            v2.vertices = [v3, v5]
+
+        and: 'D -> F'
+            v4.vertices = [v6]
+
+        and: 'E -> F'
+            v5.vertices = [v6]
+
+        and: 'F -> [G, I]'
+            v6.vertices = [v7, v9]
+
+        and: 'I -> H'
+            v9.vertices = [v8]
+
+        expect:
+            algorithm.sort(g)*.name == ['A', 'B', 'D', 'C', 'E', 'F', 'G', 'I', 'H']
+    }
+
+    def "can find the shortest-path in a weighted DAG"() {
+        given: 'a graph and a bunch of vertices'
+            TopologicalSort.Graph g = []
+            def v1 = new TopologicalSort.Vertex('A')
+            def v2 = new TopologicalSort.Vertex('B')
+            def v3 = new TopologicalSort.Vertex('C')
+            def v4 = new TopologicalSort.Vertex('D')
+            def v5 = new TopologicalSort.Vertex('E')
+            def v6 = new TopologicalSort.Vertex('F')
+            def v7 = new TopologicalSort.Vertex('G')
+            def v8 = new TopologicalSort.Vertex('H')
+            def v9 = new TopologicalSort.Vertex('I')
+            g.vertices.addAll([v9, v1, v8, v2, v7, v3, v6, v4, v5])
+
+        and: 'B -> [C(15), E(2)]'
+            v2.vertices = [v3, v5]
+            v2.edgeWeights[v3] = 15
+            v2.edgeWeights[v5] = 2
+
+        and: 'C -> I(15)'
+            v3.vertices = [v8]
+            v3.edgeWeights[v8] = 15
+
+        and: 'D -> F(1)'
+            v4.vertices = [v6]
+            v4.edgeWeights[v6] = 1
+
+        and: 'E -> F(2)'
+            v5.vertices = [v6]
+            v5.edgeWeights[v6] = 2
+
+        and: 'F -> [G(1), I(3)]'
+            v6.vertices = [v7, v9]
+            v6.edgeWeights[v7] = 1
+            v6.edgeWeights[v9] = 3
+
+        and: 'I -> H(1)'
+            v9.vertices = [v8]
+            v9.edgeWeights[v8] = 1
+
+        when:
+            def vertexToShortestPathCost = algorithm.findShortestPathsToAllOtherVertices(g, v2)
+
+        then: 'the shortest path to ourselves is 0'
+            vertexToShortestPathCost[v2] == 0
+
+        and: 'A and D are inaccessible from B, so their distance is infinite'
+            vertexToShortestPathCost[v1] == Integer.MAX_VALUE
+            vertexToShortestPathCost[v4] == Integer.MAX_VALUE
+
+        and: 'the shortest paths to the remaining elements is correct'
+            vertexToShortestPathCost[v3] == 15
+            vertexToShortestPathCost[v5] == 2
+            vertexToShortestPathCost[v6] == 4
+            vertexToShortestPathCost[v7] == 5
+            vertexToShortestPathCost[v8] == 8
+            vertexToShortestPathCost[v9] == 7
+    }
+}
