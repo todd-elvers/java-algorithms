2742c8df595540f9433115550bdabdd5ed99160d
Todd Elvers <todd.elvers@gmail.com>
2019-06-10 19:35:53 -0500

    Improved midpoint calculation such that it can no longer overflow with large values of low or high

--- src/main/java/te/interview/prep/algorithms/BinarySearch.java
+++ src/main/java/te/interview/prep/algorithms/BinarySearch.java
@@ -4,19 +4,20 @@ public class BinarySearch {
     private static final int VALUE_NOT_FOUND = -1;
 
     public int search(int[] sortedArray, int value) {
-        if(sortedArray == null) return VALUE_NOT_FOUND;
+        if (sortedArray == null) return VALUE_NOT_FOUND;
 
         int low = 0;
         int high = sortedArray.length - 1;
 
-        while(low <= high) {
-            int mid = (low + high) / 2;
+        while (low <= high) {
+            // Equivalent to (high+low)/2 but avoids possible integer overflow
+            int mid = low + ((high - low) / 2);
 
-            if(sortedArray[mid] < value) {
+            if (sortedArray[mid] < value) {
                 low = mid + 1;
-            } else if(sortedArray[mid] > value) {
+            } else if (sortedArray[mid] > value) {
                 high = mid - 1;
-            } else if(sortedArray[mid] == value) {
+            } else if (sortedArray[mid] == value) {
                 return mid;
             }
         }
