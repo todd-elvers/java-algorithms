cd3bf23630c54c8537a01d59f3ff7882f7204a04
Todd Elvers <todd.elvers@gmail.com>
2019-06-12 21:56:14 -0500

    Added/improved comments

--- src/main/java/te/interview/prep/strings_arrays/BarcodeArranger.java
+++ src/main/java/te/interview/prep/strings_arrays/BarcodeArranger.java
@@ -3,7 +3,6 @@ package te.interview.prep.strings_arrays;
 /**
  * @see <a href="https://leetcode.com/problems/distant-barcodes/">Problem on leetcode</a>
  */
-//TODO: Review this and see how everyone else did it (even though my strategy was 100%/100%)
 public class BarcodeArranger {
 
     // Time: O(n), Space: O(1)
@@ -22,9 +21,17 @@ public class BarcodeArranger {
      * for the first different value and swap it with index n-1.  This wrapping logic either
      * completes our rearrangement or moves the values to rearrange to the beginning of the array.
      * In the case where we merely moved the issue up to the front of the array a secondary call to
-     * this method will complete the rearrangement. (e.g. [2,1,1] -> [1,1,2] -> [1,2,1])
+     * this method will complete the rearrangement.
+     *
+     * e.g. [2,1,1] will become [1,1,2] after the end of our first 'rearranging' because we started
+     * at the middle element and it didn't need to be swapped because the element before it was
+     * already different, so we went to the last element and ended up having to start our search for
+     * a unique barcode from the beginning of the array.  Since we're not actually completely
+     * re-arranged yet another call to this method will get the job done and yield [1,2,1], which is
+     * the expected output. Since rearranging an array of barcodes that is already in the correct
+     * order does nothing, we always perform a secondary call to this method just in case.
      */
-    private void rearrangeBarcodes(int[] barcodes, boolean performSecondRearranging) {
+    private void rearrangeBarcodes(int[] barcodes, boolean isFirstRearrangement) {
         for (int i = 1; i < barcodes.length; i++) {
             if (barcodes[i] == barcodes[i - 1]) {
                 int swapIndex = findIndexToSwapWith(barcodes, i);
@@ -35,27 +42,21 @@ public class BarcodeArranger {
             }
         }
 
-        if (performSecondRearranging) {
-            rearrangeBarcodes(barcodes, false);
-        }
+        if (isFirstRearrangement) rearrangeBarcodes(barcodes, false);
     }
 
     private int findIndexToSwapWith(int[] barcodes, int i) {
-        // Try to find a different value between i+1 and the end of barcodes
+        // Return first differing barcode i+1 to the end of barcodes
         for (int j = i + 1; j < barcodes.length; j++) {
-            if (barcodes[i] != barcodes[j]) {
-                return j;
-            }
+            if (barcodes[i] != barcodes[j]) return j;
         }
 
-        // Try to find a different value between 0 and i-1
+        // Return first differing barcode from 0 to i-1
         for (int j = 0; j < i; j++) {
-            if (barcodes[i] != barcodes[j]) {
-                return j;
-            }
+            if (barcodes[i] != barcodes[j]) return j;
         }
 
-        return -1;
+        throw new IllegalArgumentException("Could not find any barcode aside from " + barcodes[i]);
     }
 }
 
