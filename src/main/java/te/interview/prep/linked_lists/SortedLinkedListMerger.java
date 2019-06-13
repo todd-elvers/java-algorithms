3d97a109c072b24c9a59be363f9abb89e2846faf
Todd Elvers <todd.elvers@gmail.com>
2019-06-12 21:05:15 -0500

    Corrected a comment and formatted code

--- src/main/java/te/interview/prep/linked_lists/SortedLinkedListMerger.java
+++ src/main/java/te/interview/prep/linked_lists/SortedLinkedListMerger.java
@@ -1,10 +1,5 @@
 package te.interview.prep.linked_lists;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
 import java.util.PriorityQueue;
 
 import te.interview.prep.linked_lists.domain.ListNode;
@@ -15,15 +10,15 @@ import te.interview.prep.linked_lists.domain.ListNode;
 public class SortedLinkedListMerger {
 
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists == null) return null;
-        if(lists.length == 0) return null;
-        if(lists.length == 1) return lists[0];
+        if (lists == null) return null;
+        if (lists.length == 0) return null;
+        if (lists.length == 1) return lists[0];
 
         // Space complexity = O(n) where n is the sum of the lengths of k lists
-        // Time  complexity = O(1) in the average case, O(log n) in the worst case
+        // Time  complexity = O(log n) for insert
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(ListNode node : lists) {
-            while(node != null) {
+        for (ListNode node : lists) {
+            while (node != null) {
                 minHeap.add(node.val);
                 node = node.next;
             }
@@ -31,11 +26,10 @@ public class SortedLinkedListMerger {
 
         // Time complexity = O(log n) for each operation, so O(n log n) for all operations
         ListNode head = null, tail = null;
-        while(!minHeap.isEmpty()) {
+        while (!minHeap.isEmpty()) {
             // Edge case: head & tail are null on first iteration
-            if(tail == null) {
-                tail = new ListNode(minHeap.poll());
-                head = tail;
+            if (tail == null) {
+                head = tail = new ListNode(minHeap.poll());
             } else {
                 tail.next = new ListNode(minHeap.poll());
                 tail = tail.next;
